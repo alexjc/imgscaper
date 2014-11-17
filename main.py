556a649ebc7076be02cd2a4a40eee83f031b00f0
Alex J. Champandard <alexjc@aigamedev.com>
2014-11-17 10:27:47 +0100

    Iterating until all pixels are covered, exiting when they are if no iteration count is specified.  Avoiding various coordinate clamping hacks.

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ Copyright (c) 2014, Alex J. Champandard
 """
 
 import sys
+import math
 import heapq
 import random
+import logging
 import itertools
 
 import numpy as np
@@ -17,6 +19,7 @@ import ops
 
 
 PATCH_SIZE   = 128
+PATCH_HALF   = PATCH_SIZE//2
 PATCH_MIDDLE = PATCH_SIZE//2-1
 PATCH_FINISH = PATCH_SIZE//2
 PATCH_START  = -PATCH_FINISH+1
@@ -27,6 +30,12 @@ LIGHTNESS_POWER = 0.25
 
 PATCH_COUNT = 100
 BEST_COUNT = 5
+
+
+
+def window(img):
+    return img[PATCH_HALF:-PATCH_HALF, PATCH_HALF:-PATCH_HALF]
+
 
 
 class ImageScaper(object):
@@ -50,14 +59,13 @@ class ImageScaper(object):
         self.img_luminosity = ops.rgb2hls(self.img_blurred)[:,:,1]
 
         # Now we make a histogram of the blurred luminosities, each in bins.
-        L = self.img_luminosity
+        L = window(self.img_luminosity)
         hist, bins = np.histogram(L, density=True, bins=BIN_COUNT)
         L_indices = np.digitize(L.flatten(), bins)
 
-        scipy.misc.imsave("test_lum.png", L)
-
         # Store the center of all patches by using the luminosity bins. 
-        coordinates = np.indices((source.shape[0], source.shape[1])).swapaxes(0,2).swapaxes(0,1)
+        coordinates = np.indices((source.shape[0]-PATCH_SIZE, source.shape[1]-PATCH_SIZE)).swapaxes(0,2).swapaxes(0,1)
+        coordinates += [PATCH_HALF, PATCH_HALF]
         self.c_coords = self._bin(bins, L_indices, coordinates)
 
         # For each bin we calculate the average color, per-luminosity which assumes
@@ -72,13 +80,20 @@ class ImageScaper(object):
 
         # Apply the same binning process to the spec image....
         S_indices = np.digitize(self.spec.flatten(), bins)
-        self.spec_bins = list(enumerate(S_indices))
+        self.spec_bins = {}
+        for i, bn in enumerate(S_indices):
+            # Check coordinates and discard if it's out of bounds.
+            ty, tx = i//self.spec.shape[1], i%self.spec.shape[1]
+            if ty+PATCH_START < 0 or ty+PATCH_FINISH > self.spec.shape[0]:
+                continue
+            if tx+PATCH_START < 0 or tx+PATCH_FINISH > self.spec.shape[1]:
+                continue
+            self.spec_bins[(ty, tx)] = bn
 
         # Generate a first version of the output based on the average given the luminosity
         # of the specification.  There are no interesting patterns, just colors.
         self.output = np.array([c_averages[(i-1)%BIN_COUNT] for i in S_indices], dtype=np.float32)\
                             .reshape(self.spec.shape[0], self.spec.shape[1], 3)
-        scipy.misc.imsave("test_output.png", self.output)
 
         self.coverage = np.zeros(self.output.shape[:2], dtype=np.uint8)
 
@@ -86,46 +101,44 @@ class ImageScaper(object):
         self.createMask()
 
 
-    def process(self, iterations):
+    def process(self, iterations=None):
         """Randomly pick locations to add patches to the output image, and pick the best 
         parts of the source image accordingly.
         """
 
-        count = 0
-        while True:
-            # Select a random pixel index (i) and determine its bin (bn).
-            i, bn = random.choice(self.spec_bins)
-
-            # TODO: Only select pixel indices that fall within the range of the image, 
-            # saves some extra computation and reduces complexity.
-
-            # Check coordinates and discard if it's out of bounds.
-            ty, tx = i//self.spec.shape[1], i%self.spec.shape[1]
-            if ty+PATCH_START < 0 or ty+PATCH_FINISH > self.output.shape[0]:
-                continue
-            if tx+PATCH_START < 0 or tx+PATCH_FINISH > self.output.shape[1]:
-                continue
-
-            if self.coverage[ty,tx] > 0:
-                continue
-
+        resolution = self.coverage.shape[0] - PATCH_SIZE, self.coverage.shape[1] - PATCH_SIZE
+        count, total = 0, resolution[0] * resolution[1]
+        while iterations is None or count < iterations:
             count += 1
-            if count > iterations:
+
+            # Determine pixel coverage for output image inside the target window.
+            cover = window(self.coverage)
+            ay, ax = np.where(cover == 0)
+            if len(ay) == 0:
                 break
-            sys.stdout.write("%3.1f%%\r" % (float(count+1) * 100.0 / iterations)); sys.stdout.flush();
+            
+            # Select a random pixel index (ty, tx) and determine its bin (bn).
+            i = random.randint(0, len(ay)-1)
+            ty, tx = ay[i] + PATCH_HALF, ax[i] + PATCH_HALF
+            bn = self.spec_bins[(ty,tx)]
 
             # In some cases the bins chosen may not contain any samples, in that case
             # just ignore this pixel and try again.
             if bn-1 >= BIN_COUNT-1 or len(self.c_coords[bn-1]) == 0:
+                self.coverage[ty,tx] += 1
                 continue
 
             # Find a source image patch for this target coordinate, and then splat it!
             sy, sx = self.pickBestPatch(ty, tx, self.c_coords[bn-1])
             if sx == -1 or sy == -1:
                 continue
+
             self.splatThisPatch(sy, sx, ty, tx)
 
-        # The output image as HLS can now be used in its current form, or other
+            progress = (1.0 - len(ay) / total)
+            sys.stdout.write("%3.1f%%\r" % (100.0 * progress)); sys.stdout.flush();
+
+        # The output image can now be used in its current form, or other
         # iterations may be performed.
         repro = self.output.reshape(self.spec.shape[0], self.spec.shape[1], 3)
         return repro
@@ -173,7 +186,7 @@ class ImageScaper(object):
               + self.img[sy+PATCH_START:sy+PATCH_FINISH,sx+PATCH_START:sx+PATCH_FINISH,i] * self.mask
 
         self.coverage[ty+PATCH_START:ty+PATCH_FINISH,tx+PATCH_START:tx+PATCH_FINISH] += \
-                (self.mask == 1.0)
+                (self.mask > 0.0)
 
 
     def pickBestPatch(self, ty, tx, coords):
@@ -182,12 +195,6 @@ class ImageScaper(object):
         """
         results = []
         for sy, sx in random.sample(list(coords), min(len(coords), PATCH_COUNT)):
-            # TODO: Discard in the list of coordinates those that don't fit within
-            # the image anyway, reducing complexity here.
-            if sy+PATCH_START < 0 or sy+PATCH_FINISH > self.img.shape[0]:
-                continue
-            if sx+PATCH_START < 0 or sx+PATCH_FINISH > self.img.shape[1]:
-                continue 
             d = self.D(sy, sx, ty, tx)
             heapq.heappush(results, (d, len(results), (sy,sx)))
         
@@ -209,7 +216,6 @@ def main(args):
     # The input specification is read as-is from disk, though it can be created
     # using a distance field like ops.distance() as in previous versions.
     spec = ops.normalized(scipy.misc.imread(args[0]).astype(dtype=np.float32))
-    scipy.misc.imsave("test_spec.png", spec)
 
     # Create a bigger array so there's room around the edges to apply large patches.
     # Numpy doesn't make clamping efficient or easy, so it's best just resize before starting.
@@ -221,9 +227,9 @@ def main(args):
     src = scipy.misc.imread(args[1])
 
     scraper = ImageScaper(src, spec_copy)
-    output = scraper.process(5000)
+    output = scraper.process()
 
-    scipy.misc.imsave(args[2], output[PATCH_SIZE//2:-PATCH_SIZE//2, PATCH_SIZE//2:-PATCH_SIZE//2])
+    scipy.misc.imsave(args[2], window(output))
 
 
 if __name__ == "__main__":
